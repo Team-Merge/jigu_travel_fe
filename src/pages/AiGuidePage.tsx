a5f0cf0de5546a713d935a1ae11d8159a68d3cd9
dayoon <102327475+dayoonn@users.noreply.github.com>
2025-02-07 17:37:34 +0900

    fix : 수정 챗봇 로그인 필수

--- src/pages/AiGuidePage.tsx
+++ src/pages/AiGuidePage.tsx
@@ -2,10 +2,24 @@ import React, { useState, useRef, useEffect } from "react";
 import Header from "../components/Header";
 import AiGuide from "../components/AiGuide";
 import "../styles/global.css";
+import { useNavigate } from "react-router-dom";
+const navigate = useNavigate();
+
 
 const AiGuideTest: React.FC = () => {
-   
-    
+
+    useEffect(() => {
+        const jwtToken = localStorage.getItem("jwt");
+        if (!jwtToken || jwtToken === "undefined") {
+            alert("로그인 후 사용해주세요.");
+            navigate("/auth/login");
+            return;
+        }
+    }, []);
+
+
+
+
     return (
         <div className="viewportWrapper">
             <div className="header-wrapper">
